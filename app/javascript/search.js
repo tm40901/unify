593e88dcee6e89eeb1449e8f07805861dad5e11a
tm40901 <daiki9583@gmail.com>
2024-07-21 11:58:57 +0900

    コード概要追記_search.js

--- app/javascript/search.js
+++ app/javascript/search.js
@@ -1,3 +1,8 @@
+/* コード概要 ********************************************************************************************************
+機能：点検者を選択するためのモーダルウィンドウを表示し、そのモーダル内で点検者リストを取得し、
+ユーザーが選択した点検者の情報を入力フィールドに反映する機能
+*********************************************************************************************************************/
+
 const search = () => {
   
   const inspectorList = document.getElementById('inspector_list');
@@ -6,17 +11,26 @@ const search = () => {
   const inspectorModalElement = document.getElementById('inspectorModal');
   const inspectorModal = new bootstrap.Modal(inspectorModalElement);
 
+  //モーダル表示イベントのリスナー設定
   inspectorModalElement.addEventListener('show.bs.modal', () => {
+    
     fetch('/instruments/load_inspectors')
       .then(response => {
         if (!response.ok) throw new Error('Network response was not ok');
+        // レスポンスをJSON形式に変換
         return response.json();
       })
+
       .then(data => {
+        // 点検者リストクリア
         inspectorList.innerHTML = '';
+        // 各点検者情報を表示
         data.inspectors.forEach(inspector => {
+          // ログインユーザーのデータを除外
           if (inspector.id !== data.current_user_id) {
+          // テーブル行を作成  
           const row = document.createElement('tr');
+          // 作成したテーブル行の内部HTMLを設定
           row.innerHTML = `
             <td>${inspector.employee_number}</td>
             <td>${inspector.last_name} ${inspector.first_name}</td>
@@ -25,18 +39,22 @@ const search = () => {
                         data-inspector-employee-number="${inspector.employee_number}"
                         data-inspector-name="${inspector.last_name} ${inspector.first_name}">選択</button></td>
           `;
+          // 作成したテーブル行をリストに追加
           inspectorList.appendChild(row);
           }
         });
 
+        // 選択ボタンを取得し、クリックイベントを設定
         document.querySelectorAll('.select-inspector').forEach(button => {
+          // ボタンにクリックイベントリスナーを追加
           button.addEventListener('click', (event) => {
+            // クリックされたボタンのデータ属性を取得
             const employeeNumber = event.target.getAttribute('data-inspector-employee-number');
             const name = event.target.getAttribute('data-inspector-name');
-
+            // 取得したデータを入力フィールドに設定
             inspectorEmployeeNumberField.value = employeeNumber;
             inspectorNameField.value = name;
-
+            // モーダルを非表示にする
             inspectorModal.hide();
           });
         });
